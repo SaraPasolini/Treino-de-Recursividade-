12d16908cb6a6eadf3d6dd6b61a468e89cf23381
SaraPasolini <saraalmeida3606@gmail.com>
2026-02-24 16:21:45 -0300

    Primeiro teste

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static int Calculo(int x, int y)
+    {
+        int r = 0;
+        if (y < 0) // condição de parada
+        {
+            return r = x + x;
+        }
+        return r;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o valor dos seguintes números:");
+
+        Console.Write("X :"); // Numero x representa o multiplicando
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Y :"); // Numero y representa o multiplicador
+        int y = int.Parse(Console.ReadLine());
+
+        Calculo(x, y);
+
+    }
+}
+
