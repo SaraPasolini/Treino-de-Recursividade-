e29f8dc570b3a3f32b699d78dfa3cb321d76d80f
SaraPasolini <saraalmeida3606@gmail.com>
2026-02-26 15:58:42 -0300

    Exercício 3 testado e resolvido

--- Ex1/obj/Debug/net9.0/Ex1.AssemblyInfo.cs
+++ Ex1/obj/Debug/net9.0/Ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35c614936ec0121c884870ac13aaca8e07add42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2707306b699a52b04788f30371b64f0e95be7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+   public static int Fibonacci(int n) // Precisa retornar um valor inteiro
+    {
+        if (n == 0 || n ==  1) // Condição de parada
+        {
+           return 1;
+        }
+        else  // Termo de sequencias
+        {
+            return Fibonacci (n - 1) + Fibonacci (n - 2);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos termos da série você deseja calcular : ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Fibonacci com {n} termos : ");
+
+        for (int i = 0; i <+ n; i++) // Mostrar a sequencia
+        {
+            Console.Write("{0,6}" , Fibonacci (i + 1));
+        }
+    }
+
+}
